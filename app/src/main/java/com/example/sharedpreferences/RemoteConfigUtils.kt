dd1a0d2ffdba56e87fed3395119da7fe7011ba85
LucssCarvalho <lcarvalho1@pagseguro.com>
2021-08-13 19:33:46 -0300

    refactor remote config utils

--- app/src/main/java/com/example/sharedpreferences/RemoteConfigUtils.kt
+++ app/src/main/java/com/example/sharedpreferences/RemoteConfigUtils.kt
@@ -1,11 +1,10 @@
 package com.example.sharedpreferences
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.google.firebase.remoteconfig.ktx.remoteConfig
-import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
 
 object RemoteConfigUtils {
     private const val Tag = "RemoteConfigUtils"
@@ -13,43 +12,39 @@ object RemoteConfigUtils {
     private const val HELLO_BUTTON_TEXT = "hello_button_text"
     private const val HELLO_BUTTON_COLOR = "hello_button_color"
     private const val ENABLE_BUTTON = "enable_button"
+    private const val REMOTE_CONFIG_TIMEOUT_IN_SECONDS = 30L
 
     private val DEFAULTS: HashMap<String, Any> =
         hashMapOf(
             HELLO_BUTTON_TEXT to "Hello World!!!",
-            HELLO_BUTTON_COLOR to "#0091FF"
+            HELLO_BUTTON_COLOR to "#0091FF",
+            ENABLE_BUTTON to true
         )
 
     private lateinit var remoteConfig: FirebaseRemoteConfig
 
     fun init() {
-        remoteConfig = getFirebaseRemoteConfig()
+        getFirebaseRemoteConfig()
     }
 
-    private fun getFirebaseRemoteConfig(): FirebaseRemoteConfig {
-        val remoteConfig = Firebase.remoteConfig
-
-        val configSettings = remoteConfigSettings {
-            minimumFetchIntervalInSeconds = if (BuildConfig.DEBUG) {
-                0
-            } else {
-                60 * 60
-            }
-        }
-
+    private fun getFirebaseRemoteConfig() {
+        remoteConfig = Firebase.remoteConfig
         remoteConfig.apply {
-            setConfigSettingsAsync(configSettings)
             setDefaultsAsync(DEFAULTS)
-            fetchAndActivate().addOnCompleteListener {
-                Log.d(TAG, "Remote Config Fetch Complete")
-            }
+            remoteConfig.setConfigSettingsAsync(
+                FirebaseRemoteConfigSettings.Builder()
+                    .setFetchTimeoutInSeconds(REMOTE_CONFIG_TIMEOUT_IN_SECONDS)
+                    .build()
+            )
         }
-        return remoteConfig
     }
 
-    fun getHelloButtonText(): String = remoteConfig.getString(HELLO_BUTTON_TEXT)
+    fun isEnabled(key: String): Boolean = remoteConfig.getBoolean(key)
 
-    fun getHelloButtonColor(): String = remoteConfig.getString(HELLO_BUTTON_COLOR)
-
-    fun enabledButton(): Boolean = remoteConfig.getBoolean(ENABLE_BUTTON)
+    fun blackListAppsRemoteConfig(key: String): Array<String> {
+        val blackListRemoteConfig = JSONArray(remoteConfig.getString(key))
+        return Array(blackListRemoteConfig.length()) {
+            blackListRemoteConfig.getString(it)
+        }
+    }
 }
