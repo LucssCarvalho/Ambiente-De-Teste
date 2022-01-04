dccf9fe09e151761fa6b3727e365b1d99496596d
LucssCarvalho <lcarvalho1@pagseguro.com>
2022-01-03 23:14:45 -0300

    delete class

--- app/src/main/java/com/example/sharedpreferences/Utils/FirebaseKeys.kt
+++ app/src/main/java/com/example/sharedpreferences/Utils/FirebaseKeys.kt
@@ -4,8 +4,6 @@ class FireBaseKeys {
     companion object{
         const val SHARED_PREF = "SharedPref"
         const val INPUT_NAME = "InputName"
-        const val INPUT_AGE = "Age"
-        const val TIMER = "Time"
         const val CHECKED = "Checked"
 
     }
