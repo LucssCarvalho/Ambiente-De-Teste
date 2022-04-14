6f75707aa77d78f5a31272491d3d2bbe7975833a
LucssCarvalho <lcarvalho1@pagseguro.com>
2022-04-13 21:52:39 -0300

    criacao da tela de conversa

--- app/src/main/java/com/example/chatKotlin/chat/activity/ChatActivity.kt
+++ app/src/main/java/com/example/chatKotlin/chat/activity/ChatActivity.kt
@@ -1,7 +1,7 @@
 package com.example.chatKotlin.chat.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.chatKotlin.R
 
@@ -17,7 +17,6 @@ class ChatActivity : AppCompatActivity() {
         toolbar.title = "User"
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
         setSupportActionBar(toolbar)
-
 
     }
 }
