f0586ae941d04a2082f0d43330eea579a82ee798
LucssCarvalho <lcarvalho1@pagseguro.com>
2022-04-16 15:36:00 -0300

    update preferences

--- app/src/main/java/com/example/chatKotlin/chat/helper/Preferences.kt
+++ app/src/main/java/com/example/chatKotlin/chat/helper/Preferences.kt
@@ -2,11 +2,11 @@ package com.example.chatKotlin.chat.helper
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.google.firebase.database.ValueEventListener
 
 class Preferences(context: Context) {
 
     private var CURRENT_USER_ID = "current_user_id"
+    private var CURRENT_USER_NAME = "current_user_name"
     private var CHAT_FIREBASE_ID = "chat_firebase"
     private var editor: SharedPreferences.Editor
     private var preferences: SharedPreferences
@@ -17,11 +17,13 @@ class Preferences(context: Context) {
         editor = preferences.edit()
     }
 
-    fun saveUserData(userId: String) {
+    fun saveUserData(userId: String, userName: String) {
         editor.putString(CURRENT_USER_ID, userId)
+        editor.putString(CURRENT_USER_NAME, userName)
         editor.commit()
     }
 
     fun getIdentification() = preferences.getString(CURRENT_USER_ID, null)
 
+    fun getName() = preferences.getString(CURRENT_USER_NAME, null)
 }
